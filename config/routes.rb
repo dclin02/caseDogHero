ef7372448b1de96144010b7c3943c60ef8c24458
Daniel Lin <dclin02@gmail.com>
2019-09-10 10:13:27 -0300

    Routes modified to nest dogs with clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :dogs
+  #resources :dogs
   root 'application#hello'
   resources :passeadores
-  resources :clientes# do
-   # resources :dogs
- # end
+  resources :clientes do
+    resources :dogs
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
