e434709c136c162922ad75c12a8001621788a5f1
Daniel Lin <dclin02@gmail.com>
2019-09-10 14:10:05 -0300

    Added some validations do passeadore.rb

--- app/models/passeadore.rb
+++ app/models/passeadore.rb
@@ -1,2 +1,9 @@
 class Passeadore < ApplicationRecord
+  validates_each :nome, :sobrenome do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+  end
+  # validates_each acima copiado da internet para verficação de uppercase na primeira letra dos nomes
+  validates :nome, :sobrenome, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  #validate preco
 end
