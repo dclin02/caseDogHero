40b2e5e02681f06623e88cd78bb7b2e352de4445
Daniel Lin <dclin02@gmail.com>
2019-09-11 20:22:23 -0300

    Added some unity tests for the Passeadores model and its custom validations

--- test/models/passeadore_test.rb
+++ test/models/passeadore_test.rb
@@ -4,4 +4,18 @@ class PasseadoreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save passeadore without name" do
+    passeadore = Passeadore.new
+    assert_not passeadore.save
+  end
+
+  test "should not save passeadore with whitespace only (blank) name" do
+    passeadore = Passeadore.new(:nome => "         ")
+    assert_not passeadore.save
+  end
+
+  test "should not save passeadore with name longer than 50 char" do
+    passeadore = Passeadore.new(:nome => "12345678901234567890123456789012345656778901234556")
+    assert_not passeadore.save
+  end
 end
