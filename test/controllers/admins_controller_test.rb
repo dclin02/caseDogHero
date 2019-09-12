c0724ae715335b5e635c675d0e21bcb45868e767
Daniel Lin <dclin02@gmail.com>
2019-09-12 04:55:58 -0300

    Making AdminController Tests pass auth

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,23 +1,24 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
+
   test "should get dash" do
-    get admins_dash_url
+    get admins_dash_url, headers: { Authorization: ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin') }
     assert_response :success
   end
 
   test "should get showClientes" do
-    get admins_showClientes_url
+    get admins_showClientes_url, headers: { Authorization: ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin') }
     assert_response :success
   end
 
   test "should get showDogs" do
-    get admins_showDogs_url
+    get admins_showDogs_url, headers: { Authorization: ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin') }
     assert_response :success
   end
 
   test "should get showPasseadores" do
-    get admins_showPasseadores_url
+    get admins_showPasseadores_url, headers: { Authorization: ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'admin') }
     assert_response :success
   end
 
