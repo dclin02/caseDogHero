748294c50da376370b07b1d1c9c1ba9a320dec77
Daniel Lin <dclin02@gmail.com>
2019-09-12 03:36:28 -0300

    Added Cliente search method to Welcome Controller and updated Home Page View to contain cliente email form.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+    
+  def searchCliente
+    if params[:email]
+      @cliente = Cliente.find_by(email: params[:email])
+      if @cliente.present?
+        redirect_to @cliente
+      else
+        flash[:notice] = "Cliente não encontrado."
+        redirect_to welcome_index_path
+      end
+    else
+      redirect_to welcome_index_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  get '/welcome', to: 'welcome#index'
   get 'welcome/index'
+  get 'welcome/searchCliente'
   get '/admin', to: 'admins#dash'
+  get '/admin/showClientes', to: 'admins#showClientes'
+  get '/admin/showDogs', to: 'admins#showDogs'
+  get '/admin/showPasseadores', to: 'admins#showPasseadores'
   get 'admins', to: 'admins#dash'
   get 'admins/dash'
   get 'admins/showClientes'
   get 'admins/showDogs'
   get 'admins/showPasseadores'
   #resources :dogs
-  root 'application#hello'
+  root 'welcome#index'
   resources :passeadores
   resources :clientes do
     resources :dogs
